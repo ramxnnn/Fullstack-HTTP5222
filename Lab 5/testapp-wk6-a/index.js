88023273b1cfa39750f093f5a8f9e2660ef7a194
ramxnnn <ramanpannu1003@gmail.com>
2024-10-11 23:28:32 -0400

    lab5

---
+++ Lab 5/testapp-wk6-a/index.js
@@ -0,0 +1,42 @@
+//import required modules
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const trakt = require("./modules/trakt/api");
+
+//set up Express app
+const app = express();
+const port = process.env.PORT || 8888;
+
+//define important folders
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+//setup public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//PAGE ROUTES
+app.get("/", async (request, response) => {
+  let movieList = await trakt.getTrendingMovies();
+  //console.log(movieList);
+  response.render("index", { title: "Movies", movies: movieList });
+});
+app.get("/movie/:imdbId/studios", async (request, response) => {
+  //console.log(request.params.imdbId);
+  let studioList = await trakt.getStudiosByMovieId(request.params.imdbId);
+  response.render("studios", { title: "Studios", studios: studioList });
+});
+
+// Get most 15 watched movies
+app.get("/most-watched", async (request, response) => {
+  let mostWatchedShows = await trakt.getMostWatchedShows();
+  response.render("most-watched", { title: "Top 15 Most Watched Shows", shows: mostWatchedShows });
+});
+
+//set up server listening
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+
---
+++ Lab 5/testapp-wk6-a/modules/trakt/api.js
@@ -0,0 +1,61 @@
+const trakt = "https://api.trakt.tv"; //base URL for any Trakt API requests
+
+/*
+ * Functions for Trakt API requests.
+ */
+
+//Function to retrieve a list of trending movies.
+async function getTrendingMovies() {
+  let reqUrl = `${trakt}/movies/trending?extended=full`;
+  //For the method (below), GET is the default, so if GET is your method value, you technically don't need to specify it. I put it in so you know it's there.
+  let response = await fetch(
+    reqUrl,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "trakt-api-version": 2,
+        "trakt-api-key": process.env.TRAKT_CLIENT_ID
+      }
+    }
+  );
+  return await response.json(); //return the JSON response
+}
+//Function to retrieve a list of studios by IMDB movie ID
+async function getStudiosByMovieId(imdbId) {
+  let reqUrl = `${trakt}/movies/${imdbId}/studios`;
+  let response = await fetch(
+    reqUrl,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "trakt-api-version": 2,
+        "trakt-api-key": process.env.TRAKT_CLIENT_ID
+      }
+    }
+  );
+  return await response.json();
+}
+
+async function getMostWatchedShows() {
+  let reqUrl = `${trakt}/shows/watched/period?limit=15&extended=full`; // Added limit parameter to show 15 movies only.
+  let response = await fetch(
+    reqUrl,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "trakt-api-version": 2,
+        "trakt-api-key": process.env.TRAKT_CLIENT_ID
+      }
+    }
+  );
+  return await response.json(); 
+}
+
+module.exports = {
+  getTrendingMovies,
+  getStudiosByMovieId,
+  getMostWatchedShows // added the function here to export
+};
